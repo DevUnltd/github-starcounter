e82c11a3241b062ba12d72811c392453c7b23ac2
Francisco Hodge <hello@franciscohodge.com>
2020-10-12 21:51:53 +0000

    Build update

--- build/index.js
+++ build/index.js
@@ -1,6 +1,6 @@
 /*!
  * 
- *   github-starcounter v1.0.7
+ *   github-starcounter v1.0.8
  *   https://github.com/DevUnltd/github-starcounter
  *
  *   Copyright (c) Francisco Hodge (https://github.com/hodgef)
