8e360c3e686eaa8c3fffa103dc4dd21480135b37
Francisco Hodge <hello@franciscohodge.com>
2020-10-15 20:16:28 +0000

    Build update

--- docs/dist.js
+++ docs/dist.js
@@ -1,6 +1,6 @@
 /*!
  * 
- *   github-starcounter v1.0.15
+ *   github-starcounter v1.0.16
  *   https://github.com/DevUnltd/github-starcounter
  *
  *   Copyright (c) Francisco Hodge (https://github.com/hodgef)
